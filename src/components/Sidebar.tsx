3f7288c3ebc963ffe634762c65ef32d1e7feb81d
Fabricio Orrala <fabricio.orrala@gmail.com>
2025-07-16 07:04:04 -0500

    Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,8 @@ export default function Sidebar() {
   return (
     <aside className="bg-white shadow-lg rounded-lg p-4">
       <h2 className="text-lg font-semibold mb-2 text-am">Discussion Guide</h2>
-      <p className="text-sm text-gray-600 mb-4">20-30 minutes</p>
+      <p className="text-sm bg-amber-100 text-amber-800 font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-amber-400 dark:text-amber-950 mb-4">20-30 minutes</p>
+
 
       <h3 className="font-medium text-sm mb-1">Objective</h3>
       <p className="text-xs text-gray-500 mb-4">
